8cf624a451441b621223594d41c624f0b67a0234
illei <39960324+illei@users.noreply.github.com>
2023-09-14 11:17:33 +0200

    Add files via upload

---
+++ Lab7Ex2/Lab7Ex/Lab7Ex/Autoturism.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab7Ex
+{
+    class Autoturism
+    {
+        private readonly string marca;
+        private readonly int anDeFabricatie;
+        private readonly string model;
+        private Usa usi;
+        private Motor motor;
+
+        public Autoturism(string marca, int anDeFabricatie, string model,Usa usi,Motor motor)
+        {
+            this.marca = marca;
+            this.anDeFabricatie = anDeFabricatie;
+            this.model = model;
+            this.usi = usi;
+            this.motor = motor;
+
+        }
+
+        public void PornesteMasina()
+        {
+            Console.WriteLine("Masina Porneste");
+            usi.InchideUsa();
+            motor.PornesteMotor();
+
+        }
+
+        public void OpresteMasina()
+        {
+            Console.WriteLine("Masina se opreste");
+            motor.OpresteMotor();
+            usi.DeschideUsa();
+           
+
+        }
+
+
+        public string GetDescription()
+        {
+            return $"Autoturismul este fabricat de catre :{marca}, modelul: {model},are o capacicate cilindrica de {motor.CapaticateC},este fabricat in anul {anDeFabricatie},functioneaza cu {motor.TipCombustibil}, are o putere de {motor.Putere}Cai putere, are {usi.NumarUsi} usi";
+        }
+
+
+    }
+}
---
+++ Lab7Ex2/Lab7Ex/Lab7Ex/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab7Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motor Tdi = new Motor(2000, 144, "Motorina");
+            Usa usi = new Usa(5);
+            Autoturism Golf6 = new Autoturism("VW", 2012, "Golf6", usi, Tdi);
+            Console.WriteLine(Golf6.GetDescription());
+            Golf6.PornesteMasina();
+            Golf6.OpresteMasina();
+        }
+    }
+}
